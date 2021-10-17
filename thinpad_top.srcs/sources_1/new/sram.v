c204cbcecaabc3d95c0d97b57fd57c4cfd23eb1b
hly19 <2285089396@qq.com>
2021-10-17 16:41:09 +0800

    generate bitstream successfully

--- thinpad_top.srcs/sources_1/new/sram.v
+++ thinpad_top.srcs/sources_1/new/sram.v
@@ -1,161 +1,161 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2021/10/14 16:42:54
-// Design Name: 
-// Module Name: sram
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+// `timescale 1ns / 1ps
+// //////////////////////////////////////////////////////////////////////////////////
+// // Company: 
+// // Engineer: 
+// // 
+// // Create Date: 2021/10/14 16:42:54
+// // Design Name: 
+// // Module Name: sram
+// // Project Name: 
+// // Target Devices: 
+// // Tool Versions: 
+// // Description: 
+// // 
+// // Dependencies: 
+// // 
+// // Revision:
+// // Revision 0.01 - File Created
+// // Additional Comments:
+// // 
+// //////////////////////////////////////////////////////////////////////////////////
 
 
-module sram(
+// module sram(
 
-    // 与上层连线
-    input wire clk,
-    input wire rst,
+//     // 与上层连线
+//     input wire clk,
+//     input wire rst,
 
-    input wire data_z,
+//     input wire data_z,
 
-    input wire oe, // 读使能
-    input wire we, // 写使能
-    input wire[3:0] be, // 字节使能
+//     input wire oe, // 读使能
+//     input wire we, // 写使能
+//     input wire[3:0] be, // 字节使能
 
-    input wire[19:0] addr, // 要写入的地址
-    output wire[31:0] data_in, // 读出的数据，送到上层
-    input wire[31:0] data_out, // 要写入的数据
+//     input wire[19:0] addr, // 要写入的地址
+//     output wire[31:0] data_in, // 读出的数据，送到上层
+//     input wire[31:0] data_out, // 要写入的数据
 
 
-    // 与下层连线
-    //BaseRAM信号
-    inout wire[31:0] base_ram_data_wire,  //BaseRAM数据，低8位与CPLD串口控制器共享
-    output wire[19:0] base_ram_addr, //BaseRAM地址
-    output wire[3:0] base_ram_be_n,  //BaseRAM字节使能，低有效。如果不使用字节使能，请保持为0
-    output wire base_ram_ce_n,       //BaseRAM片选，低有效
-    output wire base_ram_oe_n,       //BaseRAM读使能，低有效
-    output wire base_ram_we_n,       //BaseRAM写使能，低有效
-    //ExtRAM信号
-    inout wire[31:0] ext_ram_data_wire,  //ExtRAM数据
-    output wire[19:0] ext_ram_addr, //ExtRAM地址
-    output wire[3:0] ext_ram_be_n,  //ExtRAM字节使能，低有效。如果不使用字节使能，请保持为0
-    output wire ext_ram_ce_n,       //ExtRAM片选，低有效
-    output wire ext_ram_oe_n,       //ExtRAM读使能，低有效
-    output wire ext_ram_we_n,      //Ext RAM写使能，低有效
+//     // 与下层连线
+//     //BaseRAM信号
+//     inout wire[31:0] base_ram_data_wire,  //BaseRAM数据，低8位与CPLD串口控制器共享
+//     output wire[19:0] base_ram_addr, //BaseRAM地址
+//     output wire[3:0] base_ram_be_n,  //BaseRAM字节使能，低有效。如果不使用字节使能，请保持为0
+//     output wire base_ram_ce_n,       //BaseRAM片选，低有效
+//     output wire base_ram_oe_n,       //BaseRAM读使能，低有效
+//     output wire base_ram_we_n,       //BaseRAM写使能，低有效
+//     //ExtRAM信号
+//     inout wire[31:0] ext_ram_data_wire,  //ExtRAM数据
+//     output wire[19:0] ext_ram_addr, //ExtRAM地址
+//     output wire[3:0] ext_ram_be_n,  //ExtRAM字节使能，低有效。如果不使用字节使能，请保持为0
+//     output wire ext_ram_ce_n,       //ExtRAM片选，低有效
+//     output wire ext_ram_oe_n,       //ExtRAM读使能，低有效
+//     output wire ext_ram_we_n,      //Ext RAM写使能，低有效
     
-    output wire[15:0] leds
-    );
+//     output wire[15:0] leds
+//     );
 
-reg[31:0] base_ram_data, ext_ram_data;
-
-
-assign base_ram_data_wire = data_z ? 32'hz :base_ram_data; // 绑定输出信号
-assign ext_ram_data_wire = data_z ? 32'hz :ext_ram_data;
-
-// assign leds = data_z ?  base_ram_ce_n ? ext_ram_data_wire[15:0]: base_ram_data_wire[15:0];
-reg[15:0] led_bits;
-assign leds = led_bits;
-localparam STATE_IDLE = 4'b0000;
-localparam STATE_READ_0 = 4'b0001;
-localparam STATE_READ_1 = 4'b0010;
-localparam STATE_WRITE_0 = 4'b0011;
-localparam STATE_WRITE_1 = 4'b0100;
+// reg[31:0] base_ram_data, ext_ram_data;
 
 
-reg[3:0] state;
+// assign base_ram_data_wire = data_z ? 32'hz :base_ram_data; // 绑定输出信号
+// assign ext_ram_data_wire = data_z ? 32'hz :ext_ram_data;
 
-reg base_ram_we, base_ram_oe;
-assign base_ram_we_n = base_ram_we;
-assign base_ram_oe_n = base_ram_oe;
+// // assign leds = data_z ?  base_ram_ce_n ? ext_ram_data_wire[15:0]: base_ram_data_wire[15:0];
+// reg[15:0] led_bits;
+// assign leds = led_bits;
+// localparam STATE_IDLE = 4'b0000;
+// localparam STATE_READ_0 = 4'b0001;
+// localparam STATE_READ_1 = 4'b0010;
+// localparam STATE_WRITE_0 = 4'b0011;
+// localparam STATE_WRITE_1 = 4'b0100;
 
-reg ext_ram_we, ext_ram_oe;
-assign ext_ram_we_n = ext_ram_we;
-assign ext_ram_oe_n = ext_ram_oe;
 
-reg[31:0] data_in_reg;
-assign data_in = data_in_reg;
+// reg[3:0] state;
 
-assign base_ram_addr = addr;
-assign ext_ram_addr = addr;
+// reg base_ram_we, base_ram_oe;
+// assign base_ram_we_n = base_ram_we;
+// assign base_ram_oe_n = base_ram_oe;
 
-// assign data_in = ext_ram_ce_n ? base_ram_data_wire: ext_ram_data_wire;
+// reg ext_ram_we, ext_ram_oe;
+// assign ext_ram_we_n = ext_ram_we;
+// assign ext_ram_oe_n = ext_ram_oe;
 
-always@(posedge rst or posedge clk) begin
-    if(rst) begin
-        state <= STATE_IDLE;
-        base_ram_data <= 32'b0;
-        ext_ram_data <= 32'b0;
-        base_ram_we <= 1'b1;
-        base_ram_oe <= 1'b1;
-        ext_ram_oe <= 1'b1;
-        ext_ram_we <= 1'b1;
-        base_ram_data <= 32'b0;
-        ext_ram_data <= 32'b0;
-        led_bits <= 'b0;
-    end
+// reg[31:0] data_in_reg;
+// assign data_in = data_in_reg;
+
+// assign base_ram_addr = addr;
+// assign ext_ram_addr = addr;
+
+// // assign data_in = ext_ram_ce_n ? base_ram_data_wire: ext_ram_data_wire;
+
+// always@(posedge rst or posedge clk) begin
+//     if(rst) begin
+//         state <= STATE_IDLE;
+//         base_ram_data <= 32'b0;
+//         ext_ram_data <= 32'b0;
+//         base_ram_we <= 1'b1;
+//         base_ram_oe <= 1'b1;
+//         ext_ram_oe <= 1'b1;
+//         ext_ram_we <= 1'b1;
+//         base_ram_data <= 32'b0;
+//         ext_ram_data <= 32'b0;
+//         led_bits <= 'b0;
+//     end
     
-    else case(state)
-        STATE_IDLE: begin
-            base_ram_data <= data_out; // 上层需要处理，如果当前为读，data_out需要是z。
-            ext_ram_data <= data_out; // 选片交给上层，通过ce选.
+//     else case(state)
+//         STATE_IDLE: begin
+//             base_ram_data <= data_out; // 上层需要处理，如果当前为读，data_out需要是z。
+//             ext_ram_data <= data_out; // 选片交给上层，通过ce选.
     
-            base_ram_oe <= 1'b1;
-            ext_ram_oe <= 1'b1;
-            base_ram_we <= 1'b1;
-            ext_ram_we <= 1'b1;
-            if(we == 1'b0) begin // 写。上层点击clock_btn，（上层控制）we跳变为0，响应；如果还没点，保持
-                state <= STATE_WRITE_0;
-            end
-            else if(oe == 1'b0) begin // 读
-                state <= STATE_READ_0;
-            end
-            else begin
-                state <= STATE_IDLE;
-            end
-        end
-        STATE_WRITE_0: begin
-            base_ram_we <= 1'b0; // 信号拉低，写。
-            ext_ram_we <= 1'b0;
-            state <= STATE_WRITE_1; 
-        end
-        STATE_WRITE_1: begin
-            base_ram_we <= 1'b1; // 信号拉高
-            ext_ram_we <= 1'b1;
-            if(we == 1'b1) begin // 上层第二次点击clock_btn，（上层控制）we跳变为1，响应.如果还没点，保持.
-                state <= STATE_IDLE;
-            end
-            else begin
-                state <= STATE_WRITE_1;
-            end
-        end
-        STATE_READ_0: begin
-            base_ram_oe <= 1'b0;
-            ext_ram_oe <= 1'b0;
-            state <= STATE_READ_1;
-        end
-        STATE_READ_1: begin
-            led_bits <=  base_ram_ce_n ? ext_ram_data_wire: base_ram_data_wire;
-            if(oe == 1'b1) begin
-                state <= STATE_IDLE;
-            end
-            else begin
-                state <= STATE_READ_1;
-            end
-        end
-        default: begin
-            state <= STATE_IDLE;
-        end
-    endcase
+//             base_ram_oe <= 1'b1;
+//             ext_ram_oe <= 1'b1;
+//             base_ram_we <= 1'b1;
+//             ext_ram_we <= 1'b1;
+//             if(we == 1'b0) begin // 写。上层点击clock_btn，（上层控制）we跳变为0，响应；如果还没点，保持
+//                 state <= STATE_WRITE_0;
+//             end
+//             else if(oe == 1'b0) begin // 读
+//                 state <= STATE_READ_0;
+//             end
+//             else begin
+//                 state <= STATE_IDLE;
+//             end
+//         end
+//         STATE_WRITE_0: begin
+//             base_ram_we <= 1'b0; // 信号拉低，写。
+//             ext_ram_we <= 1'b0;
+//             state <= STATE_WRITE_1; 
+//         end
+//         STATE_WRITE_1: begin
+//             base_ram_we <= 1'b1; // 信号拉高
+//             ext_ram_we <= 1'b1;
+//             if(we == 1'b1) begin // 上层第二次点击clock_btn，（上层控制）we跳变为1，响应.如果还没点，保持.
+//                 state <= STATE_IDLE;
+//             end
+//             else begin
+//                 state <= STATE_WRITE_1;
+//             end
+//         end
+//         STATE_READ_0: begin
+//             base_ram_oe <= 1'b0;
+//             ext_ram_oe <= 1'b0;
+//             state <= STATE_READ_1;
+//         end
+//         STATE_READ_1: begin
+//             led_bits <=  base_ram_ce_n ? ext_ram_data_wire: base_ram_data_wire;
+//             if(oe == 1'b1) begin
+//                 state <= STATE_IDLE;
+//             end
+//             else begin
+//                 state <= STATE_READ_1;
+//             end
+//         end
+//         default: begin
+//             state <= STATE_IDLE;
+//         end
+//     endcase
 
-end
-endmodule
+// end
+// endmodule
--- thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -84,213 +84,186 @@ module thinpad_top(
     output wire video_de           //行数据有效信号，用于区分消隐区
 );
 
+assign ext_ram_ce_n = 1'b1; // 禁掉ext_ram
 
-// 不使用内存、串口时，禁用其使能信号
-// assign base_ram_ce_n = 1'b1;
-// assign base_ram_oe_n = 1'b1;
-// assign base_ram_we_n = 1'b1;
+// 初始状态，即reset状态，初始化寄存器并且获取addr，下一状态为读串口，注意base_ram_data为高阻态等设置。
+localparam STATE_GET_ADDRESS = 5'b00000;
 
-// assign ext_ram_ce_n = 1'b1;
-// assign ext_ram_oe_n = 1'b1;
-// assign ext_ram_we_n = 1'b1;
+// 读串口状态集合
+localparam STATE_READ_UART_1 = 5'b00001; // rdn置为1，跳转到状态2.
+localparam STATE_READ_UART_2 = 5'b00010; // 若data_ready不为1，跳转回状态1，否则跳转到状态3.
+localparam STATE_READ_UART_3 = 5'b00011; // 从base_ram_data获取数据，输出到leds，跳转到写base_ram状态.需要设置使能等。
 
-assign uart_rdn = 1'b1;
-assign uart_wrn = 1'b1;
+// 写base_ram状态集合
+localparam STATE_WRITE_BASE_1 = 5'b00100; // 准备数据和地址
+localparam STATE_WRITE_BASE_2 = 5'b00101; // 拉低写使能
+localparam STATE_WRITE_BASE_3 = 5'b00110; // 拉高写使能
+localparam STATE_WRITE_BASE_4 = 5'b00111; // 若count不为0，base_ram_data设置高阻态，跳转到读串口；否则跳转到读base_ram
+
+// 读base_ram状态集合
+localparam STATE_READ_BASE_1 = 5'b01000;  // 设置base_ram_data为高阻态，准备读数据
+localparam STATE_READ_BASE_2 = 5'b01001; // 拉低读使能，等待一段时间（或者用其他办法，保证读使能拉低足够长时间
+localparam STATE_READ_BASE_3 = 5'b01010; // 拉高读使能，获取数据，跳转到写串口
+
+// 写串口状态集合
+
+localparam STATE_WRITE_UART_1 = 5'b01011; // 将数据赋给base_ram_data，讲wrn置0
+localparam STATE_WRITE_UART_2 = 5'b01100; // 将wrn置1,
+localparam STATE_WRITE_UART_3 = 5'b01101; // 等待tbre信号变为1，变1时跳转。
+localparam STATE_WRITE_UART_4 = 5'b01110; // 等待tsre信号变为1，变1时跳转到读base_ram
 
 assign ext_ram_be_n = 'b0;
 assign base_ram_be_n = 'b0;
 
+reg[19:0] addr; // base_ram 地址
+assign base_ram_addr = addr;
 
-// reg[15:0] led_bits;
-// assign leds = led_bits;
+reg[31:0] data; // 每次读写的数据
 
-localparam STATE_GET_ADDRESS = 5'b00000;
-localparam STATE_GET_DATA = 5'b00001;
+reg [4:0] count;
 
-localparam STATE_WRITE_BASE_1 = 5'b00010;
-localparam STATE_WRITE_BASE_2 = 5'b00011;
+reg [4:0] state;
 
-localparam STATE_READ_BASE_1 = 5'b00100;
-localparam STATE_READ_BASE_2 = 5'b00101;
-
-localparam STATE_WRITE_EXT_1 = 5'b00110;
-localparam STATE_WRITE_EXT_2 = 5'b00111;
-
-localparam STATE_READ_EXT_1 = 5'b01000;
-localparam STATE_READ_EXT_2 = 5'b01001;
-
-
-reg[7:0] dp0;
-assign dpy0 = dp0;
-
-reg[7:0] dp1;
-assign dpy1 = dp0;
-
-
-reg oe;
-reg we;
-reg[3:0]be;
-reg[19:0]addr;
-reg[4:0] state;
-wire[31:0] data_in;
-reg[31:0] data_out;
-reg[3:0] count;
+reg[31:0] base_ram_data_reg; // 控制数据总线
+reg data_z;
+assign base_ram_data = data_z ? 32'hz :base_ram_data_reg;
 
 reg base_ram_ce;
 assign base_ram_ce_n = base_ram_ce;
 
-reg ext_ram_ce;
-assign ext_ram_ce_n = ext_ram_ce;
+reg rdn; // 串口读使能
+assign uart_rdn = rdn;
 
-reg data_z;
+reg wrn; // 串口写使能
+assign uart_wrn = wrn;
 
-always@(posedge clock_btn or posedge reset_btn) begin
+reg base_ram_we;
+assign base_ram_we_n = base_ram_we;
+
+reg base_ram_oe;
+assign base_ram_oe_n = base_ram_oe;
+
+always@(posedge clk_50M or posedge reset_btn) begin
     if(reset_btn) begin
-        state <= STATE_GET_ADDRESS;
-        count <= 4'b1010;
-        we <= 1'b1;
-        oe <= 1'b1;
-        be <= 'b0;
-        addr <= 'b0;
-        data_z <= 'b0;
-        data_out <= 'b0;
-        // led_bits <= 'b0;
-        ext_ram_ce <= 1'b1;
-        base_ram_ce <= 1'b1;
+        state <= STATE_READ_UART_1;
+        addr <= dip_sw[19:0]; // 获取地址
+        data_z <= 1'b1; // 设置base_ram_data为高阻态，准备读串口
+        base_ram_ce <= 1'b1; // 禁用sram
+        wrn <= 1'b1;
+        rdn <= 1'b1;
+        base_ram_oe <= 1'b1;
+        base_ram_we <= 1'b1;
+        count <= 5'b01010;
     end
-    else case(state) 
-        STATE_GET_ADDRESS: begin
-            dp0 <= 8'b11111111;
-            addr <= dip_sw[19:0];
-            state <= STATE_GET_DATA;
+    else case(state)
+        // STATE_GET_ADDRESS: begin // 没啥用
+        //     state <= STATE_READ_UART_1;
+        // end
+        STATE_READ_UART_1: begin
+            state <= STATE_READ_UART_2;
+            rdn <= 1'b1;
+            data_z <= 1'b1;
+            count <= count - 'b1;
+        end
+        STATE_READ_UART_2: begin
+            if(uart_dataready != 1'b1) begin // 串口没有准备好，跳转回状态1
+                state <= STATE_READ_UART_1;
+            end
+            else begin // 串口已经准备好
+                state <= STATE_READ_UART_3;
+                rdn <= 1'b0;
+            end
+        end
+        STATE_READ_UART_3: begin
+            state <= STATE_WRITE_BASE_1; // 下一步写内存
+            data <= base_ram_data; // 读取数据
+            rdn <= 1'b1;
+            // sram使能，使得sram可用
+            data_z <= 1'b0; 
             base_ram_ce <= 1'b0;
         end
-        STATE_GET_DATA: begin
-            data_out <= dip_sw;
-            state <= STATE_WRITE_BASE_1;
-        end
         STATE_WRITE_BASE_1: begin
-                we <= 1'b0; // 取反
-                state <= STATE_WRITE_BASE_2;
+            base_ram_data_reg <= data; // 虽然上面读到了base_ram_data上，但是考虑到改了data_z，在这里重新赋值
+            // addr 已经准备好
+            state <= STATE_WRITE_BASE_2;
         end
-        STATE_WRITE_BASE_2: begin 
-            we <= 1'b1;
-            count <= count - 1;
-            data_out <= data_out + 'b1;
-            addr <= addr + 'b1;
-            if(count == 4'b0001) begin
-                state <= STATE_READ_BASE_1;
-                count <= 4'b1010;
-                data_z <= 'b1;
-                addr <= addr - 'b1010; // 地址恢复
+        STATE_WRITE_BASE_2: begin
+            base_ram_we <= 1'b0; // 拉低写使能
+            state <= STATE_WRITE_BASE_3;
+        end
+        STATE_WRITE_BASE_3: begin // 可以考虑和2之间空一个周期
+            if(count == 5'b00000) begin // 读了10次，TODO: 可以考虑放到读的第一阶段
+                state <= STATE_READ_BASE_1; // 下一阶段，读内存
+                base_ram_we <= 1'b1; // 拉高写使能，已经写入base_ram.
+                base_ram_ce <= 1'b0; // 使能ram
+                data_z <= 1'b1;
+                count <= 5'b01010;
+                addr <= addr - 5'b01010; // 恢复addr。或者从拨码开关获得.****
             end
-            else begin 
-                state <= STATE_WRITE_BASE_1;
+            else begin
+                state <= STATE_READ_UART_1; // 不足10次，回到读串口，继续
+                base_ram_we <= 1'b1; // 拉高写使能，已经写入base_ram.
+                base_ram_ce <= 1'b1; // 禁用ram.
+                data_z <= 1'b1; // 修改总线为读状态高阻态.
+                addr <= addr + 1'b1;
             end
         end
+
         STATE_READ_BASE_1: begin
-            if(count == 4'b0000) begin
-                state <= STATE_WRITE_EXT_1;
-                // led_bits <= 'b0;
-                ext_ram_ce <= 1'b0;
-                base_ram_ce <= 1'b1;
-                count <= 4'b1010;
-                addr <= addr - 'b1001;
-                data_z <= 'b0;
-                data_out <= data_out - 32'b101;
+            base_ram_oe <= 1'b0;
+            state <= STATE_READ_BASE_2;
+            count <= count - 1'b1;
+        end
+        STATE_READ_BASE_2: begin // 空一个周期
+            state <= STATE_READ_BASE_3;
+        end
+        STATE_READ_BASE_3: begin
+            data <= base_ram_data; // 读取数据
+            base_ram_oe <= 1'b1; // 拉高使能
+            base_ram_ce <= 1'b1; // 禁用ram
+            data_z <= 1'b0; // 非高阻态
+            state <= STATE_WRITE_UART_1;
+            addr <= addr + 1'b1;
+        end
+
+        STATE_WRITE_UART_1: begin
+            base_ram_data_reg <= data;
+            wrn <= 1'b0;
+            state <= STATE_WRITE_UART_2;
+        end
+        STATE_WRITE_UART_2: begin
+            wrn <= 1'b1;
+            state <= STATE_WRITE_UART_3;
+        end
+        STATE_WRITE_UART_3: begin
+            if(uart_tbre == 1'b1) begin
+                state <= STATE_WRITE_UART_4;
             end
             else begin
-                oe <= 1'b0;
-                data_z <= 'b1;
-                state <= STATE_READ_BASE_2;
-                addr <= addr + 'b1;
+                state <= STATE_WRITE_UART_3;
             end
         end
-        STATE_READ_BASE_2: begin
-            oe <= 1'b1;
-            count <= count - 1;
-            state <= STATE_READ_BASE_1;
-            // led_bits <= data_in[15:0];
-        end
-        STATE_WRITE_EXT_1: begin
-            count <= count - 'b1;
-            we <= 1'b0; // 取反
-            state <= STATE_WRITE_EXT_2;
-        end
-        STATE_WRITE_EXT_2: begin
-            we <= 1'b1;
-            data_out <= data_out + 'b1;
-            addr <= addr + 'b1; // 地址加几位？
-            if(count == 4'b0000) begin
-                dp0 <= 8'b00000000;
-                state <= STATE_READ_EXT_1;
-                count <= 4'b1010;
-                data_z <= 'b1;
-                we <= 1'b1;
-                addr <= addr - 'b1010; // 地址恢复
-            end
-            else begin 
-                state <= STATE_WRITE_EXT_1;
-            end
-        end
-        STATE_READ_EXT_1: begin
-            if(count == 4'b0000) begin
-                count <= 4'b1010;
-                state <= STATE_READ_EXT_1;
-                // led_bits <= 'b0;
-                dp0 <= 8'b00100000;
+        STATE_WRITE_UART_4: begin
+            if(uart_tsre == 1'b1) begin
+                state <= STATE_READ_BASE_1;
+                data_z <= 1'b1; // 设置为高阻态
+                base_ram_ce <= 1'b0; // 使能ram
             end
             else begin
-                dp0 <= 8'b00001000;
-                count <= count - 1;
-                oe <= 1'b0;
-                state <= STATE_READ_EXT_2;
-                addr <= addr + 'b1;
-                // led_bits <= data_in[15:0];
+                state <= STATE_WRITE_UART_4;
             end
         end
-        STATE_READ_EXT_2: begin
-            dp0 <= 8'b10000000;
-            oe <= 1'b1;
-            state <= STATE_READ_EXT_1;
-            // led_bits <= data_in[15:0];
-        end
-        default: begin
+        default begin
             state <= STATE_GET_ADDRESS;
         end
+
+
     endcase
+
 end
 
 
-sram _sram(
-    .clk(clk_50M),
-    .rst(reset_btn),
 
-    .data_z(data_z),
 
-    .oe(oe),
-    .we(we),
-    .be(be),
-
-    .addr(addr),
-    .data_in(data_in),
-    .data_out(data_out),
-
-    .base_ram_data_wire(base_ram_data),
-    .base_ram_addr(base_ram_addr),
-    .base_ram_be_n(base_ram_be_n),
-    .base_ram_ce_n(base_ram_ce_n),
-    .base_ram_oe_n(base_ram_oe_n),
-    .base_ram_we_n(base_ram_we_n),
-
-    .ext_ram_data_wire(ext_ram_data),
-    .ext_ram_addr(ext_ram_addr),
-    .ext_ram_be_n(ext_ram_be_n),
-    .ext_ram_ce_n(ext_ram_ce_n),
-    .ext_ram_oe_n(ext_ram_oe_n),
-    .ext_ram_we_n(ext_ram_we_n),
-
-    .leds(leds)
-);
 
 endmodule
