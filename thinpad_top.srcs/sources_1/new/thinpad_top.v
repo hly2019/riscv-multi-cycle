de9f2e928b3ccf48062a5d9bdc603ca2b34de2d7
hly19 <2285089396@qq.com>
2021-10-26 13:17:30 +0800

    start stage-five, adding source files

--- thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -1,12 +1,10 @@
 `default_nettype none
 
-`include "sram.v"
-
+`include "alu.vh"
 
 module thinpad_top(
     input wire clk_50M,           //50MHz 时钟输入
     input wire clk_11M0592,       //11.0592MHz 时钟输入（备用，可不用）
-
 
     input wire clock_btn,         //BTN5手动时钟按钮开关，带消抖电路，按下时为1
     input wire reset_btn,         //BTN6手动复位按钮开关，带消抖电路，按下时为1
@@ -84,206 +82,226 @@ module thinpad_top(
     output wire video_de           //行数据有效信号，用于区分消隐区
 );
 
-assign ext_ram_ce_n = 1'b1; // 禁掉ext_ram
+    //interface to memory
+    reg         mem_oe, mem_we;
+    wire        mem_be;
+    reg[31:0]   mem_address;
+    reg[31:0]   mem_data_in;
+    wire[31:0]  mem_data_out;
+    wire        mem_done;
 
-// 初始状态，即reset状态，初始化寄存器并且获取addr，下一状态为读串口，注意base_ram_data为高阻态等设置。
+    assign mem_be = 1'b0;
+    sram _sram(
+        .clk(clk_50M),
+        .rst(reset_btn),
+        
+        .oe(mem_oe), // sram层监控这个信号，如果在初始状态为高，则转到下一个状态（否则保持）；若在最后一个状态为低，则转到初始状态，保持
+        .we(mem_we),
+        .be(mem_be),
+        
+        .address(mem_address),
+        .data_in(mem_data_in),
+        .data_out(mem_data_out),
+        .done(mem_done),
+        
+        .base_ram_data_wire(base_ram_data),
+        .base_ram_addr(base_ram_addr),
+        .base_ram_be_n(base_ram_be_n),
+        .base_ram_ce_n(base_ram_ce_n),
+        .base_ram_oe_n(base_ram_oe_n),
+        .base_ram_we_n(base_ram_we_n),
 
-// 读串口状态集合
-localparam STATE_READ_UART_1 = 5'b00001; // rdn置为1，跳转到状态2.
-localparam STATE_READ_UART_2 = 5'b00010; // 若data_ready不为1，跳转回状态1，否则跳转到状态3.
-localparam STATE_READ_UART_3 = 5'b00011; // 从base_ram_data获取数据，输出到leds，跳转到写base_ram状态.需要设置使能等。
-
-// 写base_ram状态集合
-localparam STATE_WRITE_BASE_1 = 5'b00100; // 准备数据和地址
-localparam STATE_WRITE_BASE_2 = 5'b00101; // 拉低写使能
-localparam STATE_WRITE_BASE_3 = 5'b00110; // 拉高写使能
-localparam STATE_WRITE_BASE_4 = 5'b00111; // 若count不为0，base_ram_data设置高阻态，跳转到读串口；否则跳转到读base_ram
-
-// 读base_ram状态集合
-localparam STATE_READ_BASE_1 = 5'b01000;  // 设置base_ram_data为高阻态，准备读数据
-localparam STATE_READ_BASE_2 = 5'b01001; // 拉低读使能，等待一段时间（或者用其他办法，保证读使能拉低足够长时间
-localparam STATE_READ_BASE_3 = 5'b01010; // 拉高读使能，获取数据，跳转到写串口
-
-// 写串口状态集合
-
-localparam STATE_WRITE_UART_1 = 5'b01011; // 将数据赋给base_ram_data，讲wrn置0
-localparam STATE_WRITE_UART_2 = 5'b01100; // 将wrn置1,
-localparam STATE_WRITE_UART_3 = 5'b01101; // 等待tbre信号变为1，变1时跳转。
-localparam STATE_WRITE_UART_4 = 5'b01110; // 等待tsre信号变为1，变1时跳转到读base_ram
-localparam STATE_WRITE_UART_1_5 = 5'b01111;
-assign ext_ram_be_n = 'b0;
-assign base_ram_be_n = 'b0;
-
-reg[19:0] addr; // base_ram 地址
-assign base_ram_addr = addr;
-
-reg[7:0] data; // 每次读写的数据
-
-reg [4:0] count;
-
-reg [4:0] state;
-
-reg[31:0] base_ram_data_reg; // 控制数据总线
-reg data_z;
-assign base_ram_data = data_z ? 32'hz :base_ram_data_reg;
-
-reg base_ram_ce;
-assign base_ram_ce_n = base_ram_ce;
-
-reg rdn; // 串口读使能
-assign uart_rdn = rdn;
-
-reg wrn; // 串口写使能
-assign uart_wrn = wrn;
-
-reg base_ram_we;
-assign base_ram_we_n = base_ram_we;
-
-reg base_ram_oe;
-assign base_ram_oe_n = base_ram_oe;
-
-reg[15:0] led_bits;
-assign leds = led_bits;
-
-reg [7:0]dp;
-assign dpy0 = dp;
-
-always@(posedge clk_11M0592 or posedge reset_btn) begin
-    if(reset_btn) begin
-        state <= STATE_READ_UART_1;
-        addr <= dip_sw[19:0]; // 获取地址
-        data_z <= 1'b1; // 设置base_ram_data为高阻态，准备读串口
-        base_ram_ce <= 1'b1; // 禁用sram
-        wrn <= 1'b1;
-        rdn <= 1'b1;
-        base_ram_oe <= 1'b1;
-        base_ram_we <= 1'b1;
-        count <= 5'b01010;
-        dp <= 8'b0;
+        .ext_ram_data_wire(ext_ram_data),
+        .ext_ram_addr(ext_ram_addr),
+        .ext_ram_be_n(ext_ram_be_n),
+        .ext_ram_ce_n(ext_ram_ce_n),
+        .ext_ram_oe_n(ext_ram_oe_n),
+        .ext_ram_we_n(ext_ram_we_n),
+        
+        .uart_rdn(uart_rdn),
+        .uart_wrn(uart_wrn),
+        .uart_dataready(uart_dataready),
+        .uart_tbre(uart_tbre),
+        .uart_tsre(uart_tsre)
+    );
+    
+    //interface to decoder
+    reg[31:0]       reg_instruction; // 原版指令
+    wire[5:0]       reg_s, reg_t, reg_d;
+    wire[2:0]       op;
+    wire[31:0]      imm;
+    wire            imm_select;
+    
+    decoder _decoder( // 解码得到三个寄存器编号&立即数
+        .inst           (reg_instruction),
+        .reg_s          (reg_s),
+        .reg_t          (reg_t),
+        .reg_d          (reg_d),
+        .op             (op),
+        .imm            (imm),
+        .imm_select     (imm_select)
+    );
+    
+    //interface to regfile
+    reg[4:0]            reg_waddr;
+    reg[31:0]           reg_wdata;
+    reg                 reg_we;
+    wire[31:0]          reg_rdata1;
+    wire[31:0]          reg_rdata2;
+    
+    regfile _regfile(
+        .clk            (clk_50M),
+        .rst            (reset_btn),
+        .we             (reg_we),
+        .waddr          (reg_waddr),
+        .wdata          (reg_wdata),
+        
+        .raddr1         (reg_s), // 对应解码得到的两个寄存器，得到寄存器存的值
+        .rdata1         (reg_rdata1),
+        .raddr2         (reg_t),
+        .rdata2         (reg_rdata2)
+    );
+    
+    reg[4:0]    exe_reg_d;
+    reg[2:0]    exe_op;
+    reg[31:0]   exe_imm;
+    reg         exe_imm_select;
+    
+    //interface to alu
+    reg[3:0]    alu_op;
+    reg[31:0]   exe_reg_s_val, exe_reg_t_val;
+    wire[31:0]  exe_result;
+    wire[3:0]   exe_flags;
+    always @(*) begin
+        case(exe_op)
+            `OP_LW, `OP_SW, `OP_ADD, `OP_BEQ:begin
+                alu_op <= `ADD;
+            end
+            `OP_OR: begin
+                alu_op <= `OR;
+            end
+            default: begin
+                alu_op <= `ZERO;
+            end
+        endcase
     end
-    else begin 
-    dp <= state;
-    led_bits <= count;
-    case(state)
-        // STATE_GET_ADDRESS: begin // 没啥用
-        //     state <= STATE_READ_UART_1;
-        // end
-        STATE_READ_UART_1: begin
-            
-            state <= STATE_READ_UART_2;
-            rdn <= 1'b1;
-            data_z <= 1'b1;
-        end
-        STATE_READ_UART_2: begin
-            if(uart_dataready != 1'b1) begin // 串口没有准备好，跳转回状态1
-                state <= STATE_READ_UART_1;
-            end
-            else begin // 串口已经准备好
-                state <= STATE_READ_UART_3;
-                rdn <= 1'b0;
-            end
-        end
-        STATE_READ_UART_3: begin
-            state <= STATE_WRITE_BASE_1; // 下一步写内存
-            data <= base_ram_data[7:0]; // 读取数据
-            count <= count - 'b1;
-            rdn <= 1'b1;
-            // sram使能，使得sram可用 
-            base_ram_ce <= 1'b0;
-        end
-        STATE_WRITE_BASE_1: begin
-            data_z <= 1'b0;
-            base_ram_data_reg[7:0] <= data; // 虽然上面读到了base_ram_data上，但是考虑到改了data_z，在这里重新赋值
-            // addr 已经准备好
-            state <= STATE_WRITE_BASE_2;
-        end
-        STATE_WRITE_BASE_2: begin
-            base_ram_we <= 1'b0; // 拉低写使能
-            state <= STATE_WRITE_BASE_3;
-        end
-        STATE_WRITE_BASE_3: begin
-            base_ram_we <= 1'b1; // 拉高写使能，已经写入base_ram.
-            state <= STATE_WRITE_BASE_4;
-        end
-        STATE_WRITE_BASE_4: begin // 可以考虑和2之间空一个周期
-            if(count == 5'b00000) begin // 读了10次，TODO: 可以考虑放到读的第一阶段
-                state <= STATE_READ_BASE_1; // 下一阶段，读内存
-                base_ram_we <= 1'b1; // 拉高写使能，已经写入base_ram.
-                base_ram_ce <= 1'b0; // 使能ram
-                data_z <= 1'b1;
-                count <= 5'b01010;
-                addr <= addr - 5'b01001; // 恢复addr。或者从拨码开关获得.****
-            end
-            else begin
-                state <= STATE_READ_UART_1; // 不足10次，回到读串口，继续
-                base_ram_ce <= 1'b1; // 禁用ram.
-                data_z <= 1'b1; // 修改总线为读状态高阻态.
-                addr <= addr + 1'b1;
-            end
-        end
+    
+    alu _alu(
+        .op             (alu_op),
+        .a              (exe_op == `OP_BEQ ? pc : exe_reg_s_val),
+        .b              (exe_imm_select? exe_imm: exe_reg_t_val),
+        .r              (exe_result),
+        .flags          (exe_flags)
+    );
+    
+    localparam STAGE_IF         = 3'b000;
+    localparam STAGE_ID         = 3'b001;
+    localparam STAGE_EXE        = 3'b010;
+    localparam STAGE_MEM        = 3'b011;
+    localparam STAGE_WB         = 3'b100;
 
-        STATE_READ_BASE_1: begin
-            base_ram_oe <= 1'b0;
-            state <= STATE_READ_BASE_2;
-            count <= count - 'b1;
+    reg[2:0]        cpu_stage;
+    reg[31:0]       pc;
+    
+    reg             mem_write;
+    
+    always @(posedge clk_50M or posedge reset_btn) begin
+        if (reset_btn) begin
+            cpu_stage <= STAGE_IF;
+            pc <= 32'h0;
+            reg_we <= 1'b0;
+            mem_we <= 0;
+            mem_oe <= 0;
+            mem_write <= 1'b0;
         end
-        STATE_READ_BASE_2: begin // 空一个周期
-            state <= STATE_READ_BASE_3;
-            data <= base_ram_data[7:0]; // 读取数据
-        end
-        STATE_READ_BASE_3: begin
-            base_ram_oe <= 1'b1; // 拉高使能
-            data_z <= 1'b0; // 非高阻态
-            state <= STATE_WRITE_UART_1;
-            
-        end
-        STATE_WRITE_UART_1: begin
-            base_ram_ce <= 1'b1; // 禁用ram
-            base_ram_data_reg[7:0] <= data[7:0];
-            addr <= addr + 1'b1;
-            state <= STATE_WRITE_UART_1_5;
-        end
-        STATE_WRITE_UART_1_5: begin
-            wrn <= 1'b0;
-            state <= STATE_WRITE_UART_2;
-        end
-        STATE_WRITE_UART_2: begin
-            wrn <= 1'b1;
-            state <= STATE_WRITE_UART_3;
-        end
-        STATE_WRITE_UART_3: begin
-            if(uart_tbre == 1'b1) begin
-                state <= STATE_WRITE_UART_4;
-            end
-            else begin
-                state <= STATE_WRITE_UART_3;
-            end
-        end
-        STATE_WRITE_UART_4: begin
-            if(count == 'b0) begin
-                state <= STATE_WRITE_UART_4;
+        else begin
+            case (cpu_stage)
+            STAGE_IF: begin
+                if (mem_done) begin // 成功读出指令
+                    cpu_stage <= STAGE_ID;
+                    reg_instruction <= mem_data_out; // 从内存中取出指令，送到下一阶段译码
+                    mem_oe <= 1'b0;
                 end
-            else if(uart_tsre == 1'b1) begin
-                state <= STATE_READ_BASE_1;
-                data_z <= 1'b1; // 设置为高阻态
-                base_ram_ce <= 1'b0; // 使能ram
+                else begin // 等在取指阶段，等待从内存读出指令
+                    mem_address <= pc;
+                    mem_oe <= 1'b1; // 读内存信号，开始读内存
+                end
             end
-            else begin
-                state <= STATE_WRITE_UART_4;
+            STAGE_ID: begin // 在这一阶段结束将译码的值送到alu处理，供下一阶段使用
+                cpu_stage <= STAGE_EXE;
+                exe_reg_s_val <= reg_rdata1; // 取出两个源寄存器的值，送到alu
+                exe_reg_t_val <= reg_rdata2; 
+                exe_reg_d <= reg_d; // 得到目标寄存器
+                exe_imm <= imm;
+                exe_imm_select <= imm_select;
+                exe_op <= op;
             end
+            STAGE_EXE: begin
+                case (exe_op)
+                    `OP_LW: begin
+                        cpu_stage <= STAGE_MEM;
+                        mem_write <= 1'b0; // mem_write用于告诉下一状态该指令为读或者写
+                        mem_oe <= 1'b1; // 拉高读使能，让sram读，下一周期拉低
+                        mem_address <= exe_result; // 此时alu被用来寻址
+                    end
+                    `OP_SW : begin
+                        cpu_stage <= STAGE_MEM;
+                        mem_write <= 1'b1;
+                        mem_we <= 1'b1;
+                        mem_address <= exe_result;
+                        mem_data_in <= exe_reg_t_val;
+                    end
+                    `OP_OR, `OP_ADD : begin
+                        cpu_stage <= STAGE_WB;
+                        reg_waddr <= reg_d;
+                        reg_wdata <= exe_result;
+                        reg_we <= 1'b1; // 拉高寄存器文件写使能，将alu结果写到寄存器
+                    end
+                    `OP_BEQ : begin
+                        cpu_stage <= STAGE_IF;
+                        if (exe_reg_s_val == exe_reg_t_val) // 如果两个寄存器中的值相等，跳转。
+                            pc <= exe_result;
+                        else
+                            pc <= pc + 32'h4;
+                    end
+                    default : begin
+                        cpu_stage <= STAGE_EXE; //stop
+                    end
+                endcase                
+            end
+            STAGE_MEM: begin
+                if (mem_done) begin
+                    mem_oe <= 1'b0; 
+                    mem_we <= 1'b0;
+                    cpu_stage <= STAGE_WB;
+                    if (~mem_write) begin //for memory read
+                        reg_waddr <= reg_d; // 目标寄存器的编号
+                        reg_wdata <= mem_data_out; // 读出来的值
+                        reg_we <= 1'b1; // 拉高寄存器文件写使能，将数据写入寄存器
+                    end
+                    else begin //for memory write
+                        reg_we <= 1'b0;
+                    end
+                end
+                else begin
+                    if (mem_write) begin // 保持，等待sram写完成
+                        mem_address <= exe_result;
+                        mem_we <= 1'b1;
+                        mem_oe <= 1'b0;
+                    end
+                    else begin // 保持读状态，等待读完成
+                        mem_address <= exe_result;
+                        mem_we <= 1'b0;
+                        mem_oe <= 1'b1; // oe保持拉高
+                    end
+                end
+            end
+            STAGE_WB: begin
+                cpu_stage <= STAGE_IF;
+                reg_we <= 1'b0; // 拉低寄存器文件写使能。在当前周期上升沿，reg_we为1，regfile检测到，写入，当前周期结束，reg_we拉低。
+                pc <= pc + 32'h4;
+            end
+            endcase
         end
-        default begin
-            state <= STATE_READ_UART_1;
-        end
-
-
-    endcase
     end
-
-end
-
-
-
-
-
 endmodule
